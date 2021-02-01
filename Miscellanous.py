10a85340bfe90c81e65653c5f82ce781039c10b7
rajinbound <rajinbound@gmail.com>
2021-02-01 20:58:43 +0000

    update

--- Miscellanous.py
+++ Miscellanous.py
@@ -1,16 +1,15 @@
 import requests
 
-
 #'visit-month'
-cookie = {'visit-month':'February'}
-response = requests.get('http://rahulshettyacademy.com',allow_redirects=False,cookies=cookie,timeout=1)
+cookie = {'visit-month':'December'}
+response = requests.get('http://google.com',allow_redirects=False,cookies=cookie,timeout=1)
 #301,200
 #print(response.history)
 print(response.status_code)
 
 
 se = requests.session()
-se.cookies.update({'visit-month':'February'})
+se.cookies.update({'visit-month':'January'})
 
 res = se.get("https://httpbin.org/cookies",cookies={'visit-year':'2022'})
 print(res.text)
